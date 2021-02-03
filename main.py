266a9ccc5ff8432d4787f8222c42c8ded1134c88
niramay447 <54571224+niramay447@users.noreply.github.com>
2021-02-03 23:31:44 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def next_card():
 def flip_card():
     canvas.itemconfig(card_title, text="English")
     canvas.itemconfig(card_word, text=current_card["English"])
+    canvas.itemconfig(card_background, image=card_back_img)
 
 window = Tk()
 window.title("Flashy")
@@ -25,7 +26,8 @@ window.after(3000, func=flip_card)
 canvas = Canvas(width=800, height=526)
 
 card_front_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400,263, image=card_front_img)
+card_back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400,263, image=card_front_img)
 card_title = canvas.create_text(400, 150,text="", font=("Ariel", 40,"italic"))
 card_word = canvas.create_text(400, 263,text="",font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR,highlightthickness=0)
