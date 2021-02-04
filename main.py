7529c8c57a774cf877a161b86de2007e9b3fa4d5
niramay447 <54571224+niramay447@users.noreply.github.com>
2021-02-04 19:46:36 +0530

    saving progress to a csv file
    The words that the user couldnt recollect saved to a csv file.

--- main.py
+++ main.py
@@ -4,23 +4,30 @@ import pandas
 import random
 
 data = pandas.read_csv("data/french_words.csv")
-to_learn  = data.to_dict(orient="records")
+to_learn = data.to_dict(orient="records")
 current_card = {}
+
+
 def next_card():
     global current_card, flip_timer
     window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
-    canvas.itemconfig(card_title, text="French",fill="black")
+    canvas.itemconfig(card_title, text="French", fill="black")
     canvas.itemconfig(card_word, text=current_card["French"], fill="black")
     canvas.itemconfig(card_background, image=card_front_img)
     flip_timer = window.after(3000, func=flip_card)
 
 
-# --------------------------------UI SETUP----------------------------#
 def flip_card():
     canvas.itemconfig(card_title, text="English", fill="white")
     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
     canvas.itemconfig(card_background, image=card_back_img)
+
+def is_known():
+    to_learn.remove(current_card)
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv")
+    next_card()
 
 window = Tk()
 window.title("Flashy")
@@ -31,19 +38,19 @@ canvas = Canvas(width=800, height=526)
 
 card_front_img = PhotoImage(file="images/card_front.png")
 card_back_img = PhotoImage(file="images/card_back.png")
-card_background = canvas.create_image(400,263, image=card_front_img)
-card_title = canvas.create_text(400, 150,text="", font=("Ariel", 40,"italic"))
-card_word = canvas.create_text(400, 263,text="",font=("Ariel", 60, "bold"))
-canvas.config(bg=BACKGROUND_COLOR,highlightthickness=0)
-canvas.grid(row=0,column=0,columnspan=2)
+card_background = canvas.create_image(400, 263, image=card_front_img)
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=0, column=0, columnspan=2)
 
 cross_image = PhotoImage(file="images/wrong.png")
-unknown_button = Button(image=cross_image,highlightthickness=0, command=next_card)
-unknown_button.grid(row=1,column=0)
+unknown_button = Button(image=cross_image, highlightthickness=0, command=next_card)
+unknown_button.grid(row=1, column=0)
 
 check_image = PhotoImage(file="images/right.png")
-known_button = Button(image=check_image,highlightthickness=0, command=next_card)
-known_button.grid(row=1,column=1)
+known_button = Button(image=check_image, highlightthickness=0, command=next_card)
+known_button.grid(row=1, column=1)
 
 next_card()
 
